427b1dff679d05fa01fe98fc3bd03c63ff04f4e5
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2025-02-22 06:05:23 +0000

    restyling

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+export const weatherConfig = {
+    apiKey: '12e2653bac97a3f0f5467abee99d52fd',
+    geoUrl: 'https://api.openweathermap.org/geo/1.0/direct',
+    weatherUrl: 'https://api.openweathermap.org/data/2.5/weather',
+    units: 'metric',
+    lang: 'en'
+};
